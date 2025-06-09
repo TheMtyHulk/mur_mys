c30f7d24e0c4120784ddb7b315a2bbed031f907b
pramod j <pp8063023@gmail.com>
2025-06-10 00:59:26 +0530

    Add restriction for staff members in contactLoginRequired decorator

--- helpers/decorators.py
+++ helpers/decorators.py
@@ -12,6 +12,8 @@ def anonymousRequired(view_func):
 def contactLoginRequired(view_func):
     def wrapper(request, *args, **kwargs):
         if request.user.is_authenticated:
+            if request.user.is_superuser or request.user.is_staff:
+                return HttpResponseForbidden("staff members are not allowed to access this page.")
             return view_func(request, *args, **kwargs)
         else:
             return redirect('login')  # Redirect to login page if not authenticated
