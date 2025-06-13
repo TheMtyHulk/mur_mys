6936a9f8a40a42d9c610f90529eb65e0eaf5a6c8
Pramod J <124622105+TheMtyHulk@users.noreply.github.com>
2025-06-13 13:06:31 +0530

    Update db_dict.py optimize connection string check

--- helpers/db_dict.py
+++ helpers/db_dict.py
@@ -1,14 +1,6 @@
 def parse_pg_conn_str(conn_str):
-    parts = conn_str.split()
-    conn_dict = {}
-    for part in parts:
-        if '=' in part:
-            key, value = part.split('=', 1)
-            conn_dict[key] = value
-    if conn_str:
-        return conn_dict
-    else:
-        return {
+    if not conn_str:
+         return {
             'dbname': '',
             'user': '',
             'password': '',
@@ -16,4 +8,12 @@ def parse_pg_conn_str(conn_str):
             'port': '',
             'sslmode': 'require'  # Default to require SSL
         }
+    parts = conn_str.split()
+    conn_dict = {}
+    for part in parts:
+        if '=' in part:
+            key, value = part.split('=', 1)
+            conn_dict[key] = value
     
+    return conn_dict
+   
